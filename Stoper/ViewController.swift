64569442b84d6974b5d89bfb0bccc378fc54355f
Jakub Ryszka <ryszkajakub6@gmail.com>
2022-10-26 09:25:27 +0200

    Initial Commit

---
+++ Stoper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stoper
+//
+//  Created by Jakub on 26/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
