2a17b298c1e2918bf7a37c23c93ad16a7090ee2e
Jakub Ryszka <ryszkajakub6@gmail.com>
2022-10-26 10:50:01 +0200

    add buttons to control

--- Stoper/ViewController.swift
+++ Stoper/ViewController.swift
@@ -8,23 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stopBtn: UIButton!
+    var timeStampStart = Date().timeIntervalSince1970
+    
+    var timerInterval:Timer = Timer()
 
     @IBOutlet weak var timer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        var _timerInterval = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        // Do any additional setup after loading the view
         
         
+    }
+    @IBAction func clickButton(_ sender: Any) {
+        timerInterval.invalidate()
+    }
+    @IBAction func startButtonClick(_ sender: Any) {
+        timerInterval = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
     @objc func update()
     {
         let timestamp = Date().timeIntervalSince1970
-        let dateToFormat = Date(timeIntervalSince1970: timestamp)
+        let time = timestamp - self.timeStampStart
+        let dateToFormat = Date(timeIntervalSince1970: time)
         let dF = DateFormatter();
-        dF.locale = Locale(identifier: "pl_PL")
-        dF.setLocalizedDateFormatFromTemplate("HH:mm:ss:SS")
+        
+        dF.setLocalizedDateFormatFromTemplate(" mm:ss:SS")
         let format = dF.string(from: dateToFormat)
         
         timer.text = format
