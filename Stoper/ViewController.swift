f8e26a736795bb0b8685fc69a06690104c660d7e
Jakub Ryszka <ryszkajakub6@gmail.com>
2022-10-26 10:04:31 +0200

    add timestamp

--- Stoper/ViewController.swift
+++ Stoper/ViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var _timerInterval = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        
+        
     }
+    @objc func update()
+    {
+        let timestamp = Date().timeIntervalSince1970
+        let dateToFormat = Date(timeIntervalSince1970: timestamp)
+        let dF = DateFormatter();
+        dF.locale = Locale(identifier: "pl_PL")
+        dF.setLocalizedDateFormatFromTemplate("HH:mm:ss:SS")
+        let format = dF.string(from: dateToFormat)
+        
+        timer.text = format
+    }
+    
 
 
 }
